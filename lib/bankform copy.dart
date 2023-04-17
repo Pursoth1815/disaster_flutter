0c95e5a823c039ea0e47a21b8d50fe11c517bc0e
Pursoth <pursothpersonal@gmail.com>
2023-04-17 08:13:02 +0530

    test

--- lib/bankform copy.dart
+++ lib/bankform copy.dart
@@ -1,12 +1,9 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:core';
 import 'bankservices.dart';
 import 'validation.dart';
-import 'showalert.dart';
-
 
 class BankForm extends StatefulWidget {
   BankForm({Key key}) : super(key: key);
@@ -21,11 +18,11 @@ class _BankFormState extends State<BankForm> {
 
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
- 
-  double  iconSize = 25.0;
+
+  double iconSize = 25.0;
   double fontSize = 16.0;
   FontWeight fontWeight = FontWeight.w500;
-  
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -50,10 +47,11 @@ class _BankFormState extends State<BankForm> {
                       hintText: 'Enter your Bank Name',
                       labelText: 'Bank Name',
                     ),
-                    style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+                    style:
+                        TextStyle(fontSize: fontSize, fontWeight: fontWeight),
                     inputFormatters: [new LengthLimitingTextInputFormatter(50)],
                     keyboardType: TextInputType.name,
-                    validator:  validateName,
+                    validator: validateName,
                     onSaved: (val) => newBank.bank_name = val,
                   ),
                   new TextFormField(
@@ -63,13 +61,13 @@ class _BankFormState extends State<BankForm> {
                       hintText: 'Enter your Branch Name',
                       labelText: 'Branch Name',
                     ),
-                    style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+                    style:
+                        TextStyle(fontSize: fontSize, fontWeight: fontWeight),
                     inputFormatters: [new LengthLimitingTextInputFormatter(50)],
                     keyboardType: TextInputType.name,
-                    validator:  validateName,
+                    validator: validateName,
                     onSaved: (val) => newBank.branch_name = val,
                   ),
-
                   new TextFormField(
                     initialValue: "IOBR1234567",
                     decoration: InputDecoration(
@@ -77,13 +75,13 @@ class _BankFormState extends State<BankForm> {
                       hintText: 'Enter your Bank IFSC Code',
                       labelText: 'Bank IFSC Code',
                     ),
-                    style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+                    style:
+                        TextStyle(fontSize: fontSize, fontWeight: fontWeight),
                     inputFormatters: [new LengthLimitingTextInputFormatter(16)],
                     keyboardType: TextInputType.name,
-                   validator:  validateIfsc,
+                    validator: validateIfsc,
                     onSaved: (val) => newBank.ifsc_code = val,
                   ),
-
                   new TextFormField(
                     initialValue: "12345678910",
                     decoration: InputDecoration(
@@ -91,7 +89,8 @@ class _BankFormState extends State<BankForm> {
                       hintText: 'Enter your Account number',
                       labelText: 'Account No',
                     ),
-                    style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+                    style:
+                        TextStyle(fontSize: fontSize, fontWeight: fontWeight),
                     keyboardType: TextInputType.number,
                     validator: validateAccountNo,
                     onSaved: (val) => newBank.account_no = val,
@@ -103,18 +102,17 @@ class _BankFormState extends State<BankForm> {
                       hintText: 'Application ID',
                       labelText: 'Application ID',
                     ),
-                    style: TextStyle(fontSize: fontSize, fontWeight: fontWeight),
+                    style:
+                        TextStyle(fontSize: fontSize, fontWeight: fontWeight),
                     keyboardType: TextInputType.number,
-                   // validator: validateMobile,
+                    // validator: validateMobile,
                     onSaved: (val) => newBank.ben_app_id = val,
                   ),
-
-                 
                   new Container(
                       padding: const EdgeInsets.only(left: 20.0, top: 20.0),
                       child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget> [
+                          children: <Widget>[
                             RaisedButton(
                               textColor: Colors.white,
                               color: Colors.green[900],
@@ -127,31 +125,28 @@ class _BankFormState extends State<BankForm> {
                                 child: Text('Reset'),
                                 onPressed: () {
                                   _formKey.currentState.reset();
-                                }
-                            ),
+                                }),
                           ])),
                 ],
               ))),
     );
   }
 
-
 //Snackbar Message
   void showMessage(String message, [MaterialColor color = Colors.red]) {
-    _scaffoldKey.currentState
-        .showSnackBar(new SnackBar(backgroundColor: color, content: new Text(message)));
+    _scaffoldKey.currentState.showSnackBar(
+        new SnackBar(backgroundColor: color, content: new Text(message)));
   }
 
   void _submitForm() async {
     final FormState form = _formKey.currentState;
-    
 
     if (!form.validate()) {
       showMessage('Form is not valid!  Please review and correct.');
     } else {
       form.save(); //This invokes each onSaved event
-      //showMessage("Processing Data"); 
-    
+      //showMessage("Processing Data");
+
       print('Form save called...');
       print('Bank Name: ${newBank.bank_name}');
       print('Branch Name: ${newBank.branch_name}');
@@ -164,35 +159,32 @@ class _BankFormState extends State<BankForm> {
 //     Map<String,String> mapData = {"bank_name":"${newBank.bank_name}","branch_name":"${newBank.branch_name}","ifsc_code":"${newBank.ifsc_code}","account_no":"${newBank.account_no}","ben_app_id":"${newBank.ben_app_id}"};
 //      print("something");
 //      String url;
-//     if (Platform.isAndroid) 
+//     if (Platform.isAndroid)
 //     url = "http://10.0.2.2:3002/save_bankdetails";
-//     else 
+//     else
 //     url ="http://localhost:3002/save_bankdetails";
-   
+
 //    Map<String, String> headers = {
 //       "Content-type": "application/x-www-form-urlencoded"
 //     };
 //     String url1 = "http://10.0.2.2:3002/save_bankdetails";
 //    // String url1 = "http://localhost:3002/save_bankdetails";
 
-//   http.Response response = await http.post(url1, 
-//   headers: headers, 
+//   http.Response response = await http.post(url1,
+//   headers: headers,
 //   body : mapData,
 //  //body: {"bank_name":"Indian Overseas Bank","branch_name":"Rajaji Bhavan","ifsc_code":"IOBR1234567","account_no":"12345678910","ben_app_id":"1234"}, // working code
 //   );
- 
+
 //    print("response 1");
 //     print("API Response ");
 //     print(response.statusCode);
 //     print(response.body);
-  
-  
- 
+
       // bankService.createBank(mapData);
       bankService.createBank(newBank);
 
-     // showAlertDialog(context, "Success", "Bank Details Submitted"); // Reusable alert Dialog
+      // showAlertDialog(context, "Success", "Bank Details Submitted"); // Reusable alert Dialog
     }
-    
   }
 }
